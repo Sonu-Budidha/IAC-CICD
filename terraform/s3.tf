59997630c84c9fcbc3af640723e4e968a284485f
Sonu Budidha <shravu800@gmail.com>
2025-12-09 04:23:25 +0530

    IAC+CICD3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-uniqdsdsdsdsddsdsdsdsdsdsdsdsdsdfdghfgdh"
+  bucket = "my-uniqdsdfhdgfhdghdfgsdsdsddsdsdsdsdsdsdsdsdsdfdghfgdh"
   acl    = "private"
 }
 
