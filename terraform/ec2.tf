c69fb200f6390dab35757dbe1acf5751cf805cb6
Sonu Budidha <shravu800@gmail.com>
2025-12-09 04:02:39 +0530

    IAC+CICD

---
+++ terraform/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = "my-ssh-key"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  # Install Docker & run ECR image
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              ./aws/install
+              $(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${aws_ecr_repository.flask_repo.repository_url})
+              docker pull ${aws_ecr_repository.flask_repo.repository_url}:latest
+              docker run -d -p 80:8080 ${aws_ecr_repository.flask_repo.repository_url}:latest
+              EOF
+
+  depends_on = [aws_ecr_repository.flask_repo]
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
---
+++ terraform/security.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db-sg"
+  description = "Allow MySQL from EC2"
+}
+
+resource "aws_security_group_rule" "db_inbound_mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_sg.id
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
