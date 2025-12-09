3804085a8c556a76a8bd3c9022560c057f026c1d
Sonu Budidha <shravu800@gmail.com>
2025-12-10 03:20:57 +0530

    first cogfgfmmit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hfhdfgdhfgdhfgdhgdhgdhgdj"
+    bucket         = "hfhdfgdhfgdhfgdhgddsdsdsdsdsdhgdhdssdgdj"
     key            = "project/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-locks"
