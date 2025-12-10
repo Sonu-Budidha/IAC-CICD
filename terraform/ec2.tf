99d1314a335c03fd2cd0d4d210b9ea52685d6101
Sonu Budidha <shravu800@gmail.com>
2025-12-11 04:26:30 +0530

    ffffdfIAC+CICDf3

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,23 +5,14 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install docker* -y
-              sudo systemctl start docker
-              sudo usermod -a -G docker ec2-user
-              
 	      # Install AWS CLI v2
               curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
               unzip awscliv2.zip
               ./aws/install
 
               # Login to ECR
-              aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${aws_ecr_repository.flask_repo.repository_url}
+              #aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${aws_ecr_repository.flask_repo.repository_url}
               
-              # Pull and run latest image
-              #docker pull ${aws_ecr_repository.flask_repo.repository_url}:latest
-              #docker run -d -p 8080:8080 ${aws_ecr_repository.flask_repo.repository_url}:latest
               EOF
 
   depends_on = [aws_ecr_repository.flask_repo]
